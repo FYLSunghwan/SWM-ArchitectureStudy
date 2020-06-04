4f3539cfc64cd5c8e71dec9d5f2bb53b5bef4fe8
FYLSunghwan <sunghwan519@hotmail.com>
2020-06-04 09:34:18 +0900

    feat: Add dispacher method

--- Part_2_1_Dispatcher_Pattern/BasicServer/src/Dispatcher.java
+++ Part_2_1_Dispatcher_Pattern/BasicServer/src/Dispatcher.java
@@ -1,4 +1,14 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Dispatcher {
 
+	public void dispatch(ServerSocket serverSocket) {
+		try {
+			Socket socket = serverSocket.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
