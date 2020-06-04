34b811f7e25af350d53c1d42a16fb4fe0c6133c9
FYLSunghwan <sunghwan519@hotmail.com>
2020-06-04 09:47:27 +0900

    add: Add StreamSayHelloProtocol

---
+++ Part_2_1_Dispatcher_Pattern/BasicServer/src/StreamSayHelloProtocol.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.StringTokenizer;
+
+public class StreamSayHelloProtocol {
+	
+	private static final int DATA_SIZE = 512;
+	private static final int TOKEN_NUM = 2;
+
+	public void handleEvent(InputStream inputStream) {
+		try {
+			byte[] buffer = new byte[DATA_SIZE];
+			inputStream.read(buffer);
+			String data = new String(buffer);
+			
+			String[] params = new String[TOKEN_NUM];
+			StringTokenizer token = new StringTokenizer(data, "|");
+			
+			int i = 0;
+			while(token.hasMoreTokens()) {
+				params[i] = token.nextToken();
+				++i;
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
