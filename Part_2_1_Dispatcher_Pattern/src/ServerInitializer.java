a67c63b87224b1943ee6e1c39077cf17427763ad
FYLSunghwan <sunghwan519@hotmail.com>
2020-06-04 09:19:01 +0900

    feat: Add server

--- Part_2_1_Dispatcher_Pattern/src/ServerInitializer.java
+++ Part_2_1_Dispatcher_Pattern/src/ServerInitializer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,6 +13,15 @@ public class ServerInitializer {
 		try {
 			ServerSocket serverSocket = new ServerSocket(port);
 			Socket connection;
+			
+			while(true) {
+				connection = serverSocket.accept();
+				InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
+				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+				String line = bufferedReader.readLine();
+				
+				System.out.println("READ:" + line);
+			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
