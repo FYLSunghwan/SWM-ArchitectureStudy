3c676ee108ef71cb9dc764f9fc48b8659cf26af8
FYLSunghwan <sunghwan519@hotmail.com>
2020-06-04 10:24:18 +0900

    env: Duplicate project

---
+++ Part_2_1_Dispatcher_Pattern/ReactorServer/src/ServerInitializer.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerInitializer {
+
+	public static void main(String[] args) {
+		int port = 5000;
+		System.out.println("Server ON :"+port);
+		
+		try {
+			// 서버 설정
+			ServerSocket serverSocket = new ServerSocket(port);
+			Dispatcher dispatcher = new Dispatcher();
+			
+			while(true) {
+				dispatcher.dispatch(serverSocket);
+			}
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
